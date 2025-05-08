55aa0f87d24193d61fdcb8feb88584b2dae2fc2c
Antonio Oliveira <antoniovini47@gmail.com>
2025-05-08 16:26:03 -0300

    Sending emoji as message working v0.1

--- content.js
+++ content.js
@@ -18,6 +18,7 @@ let isActivated = false;
 
 startButton.addEventListener("click", () => {
   isActivated = !isActivated;
+  startObserver();
   startButton.style.backgroundColor = isActivated ? "lightgreen" : "white";
   console.log(`Button is now ${isActivated ? "activated" : "deactivated"}`);
 });
@@ -27,8 +28,6 @@ function onNewMessage() {
     sendReactionAsAMessage();
   }
 }
-
-setInterval(onNewMessage, 5000);
 
 async function sendReactionAsAMessage() {
   console.log("Checking messages...");
@@ -46,6 +45,10 @@ async function sendReactionAsAMessage() {
   await new Promise((resolve) => setTimeout(resolve, pauseTime));
 
   const bees = document.querySelectorAll(`[data-emoji="${reaction}"]`);
+  if (bees.length === 0) {
+    console.log("No bees found, skipping");
+    return;
+  }
   bees.forEach((bee) => {
     console.log("Clicking on the bee");
     bee.click();
@@ -54,7 +57,52 @@ async function sendReactionAsAMessage() {
   await new Promise((resolve) => setTimeout(resolve, pauseTime));
 
   console.log("Clicking on the send button");
-  const sendButton = document.querySelector('[aria-label="Send"]');
+  // "Send" or "Enviar"
+  const sendButton =
+    document.querySelector('[aria-label="Send"]') ||
+    document.querySelector('[aria-label="Enviar"]');
+  if (!sendButton) {
+    console.log("No send button found, skipping");
+    return;
+  }
   sendButton.click();
   console.log(`Message ${reaction} sent`);
+}
+
+const observer = new MutationObserver(() => {
+  if (!isActivated) {
+    console.log("Not activated, skipping");
+    return;
+  }
+  console.log("Activated, checking messages");
+  const messages = document.querySelectorAll("div[role='row']");
+  console.log(`Found ${messages.length} messages`);
+  const lastMessageDiv = messages[messages.length - 1];
+  console.log("Last message div:", lastMessageDiv);
+  sendReactionAsAMessage();
+  disableTemporaryTheScript();
+});
+
+async function disableTemporaryTheScript() {
+  isActivated = false;
+  setTimeout(() => {
+    isActivated = true;
+  }, 2000);
+}
+
+async function startObserver() {
+  if (!isActivated) {
+    console.log("Not activated, skipping");
+    return;
+  }
+  console.log("Waiting for 1 seconds to garantee the whatsapp page is loaded. Select the chat!!!");
+  setTimeout(() => {
+    const chatContainer = document.querySelector('[role="application"]');
+    console.log("Chat container:", chatContainer);
+    if (chatContainer) {
+      observer.observe(chatContainer, { childList: true, subtree: true });
+    } else {
+      console.log("No chat container found, skipping");
+    }
+  }, 1000);
 }
